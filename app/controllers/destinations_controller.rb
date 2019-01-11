038185e596a12f007a8f73c08551d1487e87fd26
ChrisMFernandes <Chrismfernandes12@gmail.com>
2019-01-11 17:07:49 -0500

    active storage done

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -10,15 +10,22 @@ class DestinationsController < ApplicationController
 
   def create
     @destination = Destination.create(destination_params)
+    
+    if @destination.save
+      redirect_to @destination
+    end
+
   end
 
   def show
+    @destination = Destination.find(params[:id])
   end
 
 
   private
 
   def destination_params
-    params.require(:destination).permit(:title, :body, images: [])
+    params.require(:destination).permit(:title, :body, :image)
   end
+
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,3 @@
 class Destination < ApplicationRecord
-  has_many_attached :images
+  has_one_attached :image
 end
