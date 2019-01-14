9ddbd90747529fda167a0626e0c84eff123d1f2d
ChrisMFernandes <Chrismfernandes12@gmail.com>
2019-01-13 19:39:15 -0500

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  root 'home#index'
+  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+
   resources :albums
   resources :destinations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
