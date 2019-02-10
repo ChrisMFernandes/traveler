086e99528a132f82f78f45e01bc18af5c0574cb1
ChrisMFernandes <Chrismfernandes12@gmail.com>
2019-02-10 17:28:57 -0500

    adding some stuff to dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,13 @@
 class HomeController < ApplicationController
+  include HomeHelper
   
   def index
     render layout: false
   end
 
   def dashboard
+    @destinations = Destination.order(created_at: :desc).limit(3)
+    @image = random_image(@destinations)
     render layout: false
   end
 
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,10 @@
 module HomeHelper
+
+  def random_image(destinations)
+    destinations.each do |dest|
+      dest.albums.each do |album|
+        return album.images[0]
+      end
+    end
+  end
 end
