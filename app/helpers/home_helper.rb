061b6addc287f5e1b9bb7d8744e3bf6fd3b0b0b4
ChrisMFernandes <Chrismfernandes12@gmail.com>
2020-01-07 07:12:50 -0500

    updated gems

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,10 +1,10 @@
 module HomeHelper
 
-  def random_image(destinations)
-    destinations.each do |dest|
-      dest.albums.each do |album|
-        return album.images[0]
-      end
+  def random_image(destination)
+    if destination.albums?
+        destination.albums.each do |album|
+          return album.images[0]
+        end
     end
   end
 end
